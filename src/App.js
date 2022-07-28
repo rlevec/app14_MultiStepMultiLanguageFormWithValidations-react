9e4654b7a5451834b13e5fae093ade0abfbbc4bb
rlevec <rene.levec@outlook.com>
2022-07-28 11:57:04 +0200

    init

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import { AppContext  } from './context'
 import PropagateLoader from "react-spinners/PropagateLoader";
 
 
+
 const App = () => {
   const { count, setCount, english, spanish, selectedMonth, setSelectedMonth, selectedYear, setSelectedYear, showTerms, setShowTerms, initialValues, formValues, formErrors, isSubmit, handleChange, handleSubmit, handleEnglish, handleSpanish, loading, setLoading} = useContext(AppContext)
 
   const rightNavContainerRef = useRef(null)
   const leftNavContainerRef = useRef(null)
+
+  const [loadingStep, setLoadingStep] = useState(false)
   
 
   useEffect(() => {
@@ -23,6 +26,14 @@ const App = () => {
     }, 3000)
     return () => clearTimeout(timeOut)
   }, [])
+
+  useEffect(() => {
+    setLoadingStep(true)
+    let timeOut = setTimeout(() => {
+      setLoadingStep(false)
+    }, 3000)
+    return () => clearTimeout(timeOut)
+  }, [count])
 
  
 
@@ -147,105 +158,121 @@ const App = () => {
               ): null}
             {count === 2 ? (
                 <>
-                  <div className='form-group form-transition'>
-                    <label className='form-label'>{english && 'Username'}{spanish && 'Nombre de Usuario'}</label>
-                    <input 
-                      type='text'
-                      className='form-control'
-                      name='username'
-                      errorMessage=''
-                      onChange={handleChange} 
-                      value={formValues.username}
-                    />
-                  </div> 
-                  <p className='error-message'>{formErrors.username}</p>
-                  <div className='form-group'>
-                    <label className='form-label'>Email</label>
-                    <input 
-                      type='email'
-                      className='form-control'
-                      name='email'
-                      errorMessage='It should be a valid email address!'
-                      onChange={handleChange} 
-                      value={formValues.email}
-                    />
-                  </div>
-                  <p className='error-message'>{formErrors.email}</p> 
-                  <div className='form-group'>
-                    <label className='form-label'>{english && 'Password'}{spanish && 'La Contraseña'}</label>
-                    <input 
-                      type='password'
-                      className='form-control'
-                      name='password'
-                      errorMessage='Password should be 8-12 characters and it should include at least 1 letter 1 number and 1 special character!'
-                      onChange={handleChange} 
-                      value={formValues.password}
-                    />
-                  </div> 
-                  <p className='error-message'>{formErrors.password}</p>
-                  <div className='form-group'>
-                    <label className='form-label'>{english && 'Confirm Password'}{spanish && 'Confirmar Contraseña'}</label>
-                    <input 
-                      type='password'
-                      className='form-control'
-                      name='confirmPassword'
-                      errorMessage='Passwords do not match!'
-                      onChange={handleChange} 
-                      value={formValues.confirmPassword}
-                    />
-                  </div> 
-                  <p className='error-message'>{formErrors.confirmPassword}</p>
+                  { loadingStep ? (
+                  <PropagateLoader className='loader-step1' size={30} color={"#002D62"} loading={loadingStep}
+                />) : (
+                  <div>
+                    
+                    <div className='form-group form-transition'>
+                      <label className='form-label'>{english && 'Username'}{spanish && 'Nombre de Usuario'}</label>
+                      <input 
+                        type='text'
+                        className='form-control'
+                        name='username'
+                        errorMessage=''
+                        onChange={handleChange} 
+                        value={formValues.username}
+                      />
+                    </div> 
+                    <p className='error-message'>{formErrors.username}</p>
+                    <div className='form-group'>
+                      <label className='form-label'>Email</label>
+                      <input 
+                        type='email'
+                        className='form-control'
+                        name='email'
+                        errorMessage='It should be a valid email address!'
+                        onChange={handleChange} 
+                        value={formValues.email}
+                      />
+                    </div>
+                    <p className='error-message'>{formErrors.email}</p> 
+                    <div className='form-group'>
+                      <label className='form-label'>{english && 'Password'}{spanish && 'La Contraseña'}</label>
+                      <input 
+                        type='password'
+                        className='form-control'
+                        name='password'
+                        errorMessage='Password should be 8-12 characters and it should include at least 1 letter 1 number and 1 special character!'
+                        onChange={handleChange} 
+                        value={formValues.password}
+                      />
+                    </div> 
+                    <p className='error-message'>{formErrors.password}</p>
+                    <div className='form-group'>
+                      <label className='form-label'>{english && 'Confirm Password'}{spanish && 'Confirmar Contraseña'}</label>
+                      <input 
+                        type='password'
+                        className='form-control'
+                        name='confirmPassword'
+                        errorMessage='Passwords do not match!'
+                        onChange={handleChange} 
+                        value={formValues.confirmPassword}
+                      />
+                    </div> 
+                    <p className='error-message'>{formErrors.confirmPassword}</p>
+                  </div>)}
                 </>
             ): null}
             {count === 3 ? (
               <>
+              { loadingStep ? (
+                  <PropagateLoader className='loader-step1' size={30} color={"#002D62"} loading={loadingStep}
+                />) : (
+                  <div>
+                    <div className='form-group'>
+                      <label className='form-label'>{english && 'Credit Card Number (AMEX)'}{spanish && 'Número de Tarjeta de Crédito (AMEX)'}</label>
+                      <input 
+                        type="tel" 
+                        inputmode="numeric" 
+                        placeholder=""
+                        className='form-control'
+                        name='creditCard'
+                        onChange={handleChange} 
+                        value={formValues.creditCard}
+                      />
+                  </div> 
+                  <p className='error-message'>{formErrors.creditCard}</p>
+                  <div className='mm-yy-dropdown-container'>
+                    <MonthDropdown selectedMonth={selectedMonth} setSelectedMonth={setSelectedMonth} english={english} spanish={spanish}/>
+                    <YearDropdown selectedYear={selectedYear} setSelectedYear={setSelectedYear} english={english} spanish={spanish}/>
+                  </div>
                   <div className='form-group'>
-                    <label className='form-label'>{english && 'Credit Card Number (AMEX)'}{spanish && 'Número de Tarjeta de Crédito (AMEX)'}</label>
+                    <label className='form-label'>CVV</label>
                     <input 
-                      type="tel" 
-                      inputmode="numeric" 
-                      placeholder=""
+                      type='text'
                       className='form-control'
-                      name='creditCard'
+                      name='cvn'
                       onChange={handleChange} 
-                      value={formValues.creditCard}
+                      value={formValues.cvn}
                     />
-                </div> 
-                <p className='error-message'>{formErrors.creditCard}</p>
-                <div className='mm-yy-dropdown-container'>
-                  <MonthDropdown selectedMonth={selectedMonth} setSelectedMonth={setSelectedMonth} english={english} spanish={spanish}/>
-                  <YearDropdown selectedYear={selectedYear} setSelectedYear={setSelectedYear} english={english} spanish={spanish}/>
-                </div>
-                <div className='form-group'>
-                  <label className='form-label'>CVV</label>
-                  <input 
-                    type='text'
-                    className='form-control'
-                    name='cvn'
-                    onChange={handleChange} 
-                    value={formValues.cvn}
-                  />
-                </div> 
-                <p className='error-message'>{formErrors.cvn}</p>
+                  </div> 
+                  <p className='error-message'>{formErrors.cvn}</p>
+                </div>)}
               </>
             ): null}
             {count === 4 ? (
-              <div className='terms-body'>
-                  <div className='terms-box'>
-                  <div className='terms-text'>
-                    <h2>{english && 'terms of service'}{spanish && 'términos de servicio'}</h2>
-                    <p>{english && 'Last Edit: 07/26/2022'}{spanish && 'Última Edición: 26/07/2022'}</p>
-                    <p>{english && 'Greetings Users,'}{spanish && 'Saludos Usuarios,'}</p>
-                    {english && terms}{spanish && spanishTerms}
+              <>
+              { loadingStep ? (
+                  <PropagateLoader className='loader-step1' size={30} color={"#002D62"} loading={loadingStep}
+                />) : (
+                <div className='terms-body'>
+                    <div className='terms-box'>
+                    <div className='terms-text'>
+                      <h2>{english && 'terms of service'}{spanish && 'términos de servicio'}</h2>
+                      <p>{english && 'Last Edit: 07/26/2022'}{spanish && 'Última Edición: 26/07/2022'}</p>
+                      <p>{english && 'Greetings Users,'}{spanish && 'Saludos Usuarios,'}</p>
+                      {english && terms}{spanish && spanishTerms}
+                    </div>
+                    <h3>{english && 'I agree to the '}{spanish && 'Estoy de acuerdo con la'}<span>{english && 'Terms of Service'}{spanish && 'Términos de Servicio'}</span>{english && ' and I read the Privacy Notice.'}{spanish && ' y leí el Aviso de Privacidad.'}</h3>
+                    <div className='terms-buttons'>
+                      {showTerms && count===4 && <button type='submit' className='submit-btn'>{english && 'Submit'}{spanish && 'Enviar'}</button>}
+                      <button className='btn-accept red-btn' onClick={() => setShowTerms(true)}>{english && 'Accept'}{spanish && 'Aceptar'}</button>
+                      <button className='btn-decline gray-btn' onClick={() => setShowTerms(false)}>{english && 'Decline'}{spanish && 'Rechazar'}</button>
+                    </div>
                   </div>
-                  <h3>{english && 'I agree to the '}{spanish && 'Estoy de acuerdo con la'}<span>{english && 'Terms of Service'}{spanish && 'Términos de Servicio'}</span>{english && ' and I read the Privacy Notice.'}{spanish && ' y leí el Aviso de Privacidad.'}</h3>
-                  <div className='terms-buttons'>
-                    {showTerms && count===4 && <button type='submit' className='submit-btn'>{english && 'Submit'}{spanish && 'Enviar'}</button>}
-                    <button className='btn-accept red-btn' onClick={() => setShowTerms(true)}>{english && 'Accept'}{spanish && 'Aceptar'}</button>
-                    <button className='btn-decline gray-btn' onClick={() => setShowTerms(false)}>{english && 'Decline'}{spanish && 'Rechazar'}</button>
-                  </div>
-                </div>
-              </div>
+                </div>)}
+              </>
             ): null}
             {(count === 4) ? (
               <button 
